810b3d5ac97bb524234ed6cd340d8b239a42dfc3
Thuy <tpham045@hotmail.com>
2017-12-02 00:13:56 +0000

    Testing updating front end

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import {render} from 'react-dom';
 const rootElement = document.getElementById('main');
 
 render(
-  <div><h1>Our website is under construction. We'll be here soon with awesome site.</h1>
+  <div><h1>Our website is under construction. We will be here soon with awesome site.</h1>
   <h2>Contact us through <a href='https://www.facebook.com/toursforbooks/app/138037989567125/'>Facebook</a></h2></div>,
   rootElement);
